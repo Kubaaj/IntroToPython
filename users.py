5473e1a5d561cfca70484608b3e579441c830ce7
jery33 <jerzykb95@gmail.com>
2019-01-05 11:44:06 +0100

    NEW: - forgot password feature

--- users.py
+++ users.py
@@ -1,1 +1,7 @@
-users={"admin@admin.com":{"name":"Admin Admin", "password":"admin", "email":"admin@admin.com", "loggedIn":False,  "randStr":"", "lastSeen":0}}
+users={"admin@admin.com":
+{"name":"Admin Admin",
+ "password":"admin",
+ "email":"admin@admin.com",
+ "loggedIn":False,
+ "randStr":"",
+ "lastSeen":0}}
