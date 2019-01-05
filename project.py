6a33d72c750a9a1a7ad7cb86dd320911e46c891b
Kubaaj <jakubajchel@gmail.com>
2019-01-05 12:44:06 +0100

    Add DataBase

---
+++ project.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  2 18:54:10 2019
+
+@author: jakub
+"""
+#import libraries
+import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns 
+
+#import dataset
+#movies = pd.read_excel('C:/Users/jakub/Desktop/IntroToPython_2/IntroToPython/the-movies-dataset/movies_metadata.xlsx', index_col=None, header=0)
+
+
+# Check the first 5 rows of the data frame
+movies.head()
+
