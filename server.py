ad4b793c3b7f9de3bc048f4e04d29ce0fadef4f5
Kubaaj <jakubajchel@gmail.com>
2019-01-21 22:53:36 +0100

    UPDATE

--- server.py
+++ server.py
@@ -853,7 +853,9 @@ def checkAuth():
 
 
 
-run(host='localhost', port=8080, debug=True)
-
+if os.environ.get('APP_LOCATION') == 'heroku':
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+    run(host='localhost', port=8080, debug=True)
 
 #clean up
